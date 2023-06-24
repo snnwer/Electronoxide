81839a46e4ff47132a48b1ea42592ba61231aa91
snnwer <75314624+snnwer@users.noreply.github.com>
2023-06-24 20:04:46 +0500

    Update ScriptScanner.lua

--- modules/ScriptScanner.lua
+++ modules/ScriptScanner.lua
@@ -23,7 +23,7 @@ local function scan(query)
                 not scripts[script] and 
                 script:IsA("LocalScript") and 
                 script.Name:lower():find(query) and
-                (success and not result:match('bytecode')) and
+                success and typeof(result) == 'function' and
                 pcall(function() getsenv(script) end)
             then
                 scripts[script] = LocalScript.new(script)
